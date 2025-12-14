d9c27d4194b52d7ee5f0c05aca04739c5f54f67f
nazeh <nuh@nuh.dev>
2025-12-14 22:59:26 +0300

    test: simple two inputs test

--- tests/validate.rs
+++ tests/validate.rs
@@ -32,3 +32,35 @@ fn single_input() {
 
     validate(&tx, &prevouts, &scripts).unwrap();
 }
+
+#[test]
+fn two_inputs() {
+    let script_witnesses = "OP_RETURN OP_PUSHBYTES_2 0100 OP_PUSHBYTES_3 010101";
+
+    let tx = Transaction {
+        version: bitcoin::transaction::Version::TWO,
+        lock_time: bitcoin::locktime::absolute::LockTime::ZERO,
+        input: vec![
+        // Skip adding an input, since we are not using sighash
+        ],
+        output: vec![TxOut {
+            value: Amount::ZERO,
+            script_pubkey: ScriptBuf::from_asm(script_witnesses).unwrap(),
+        }],
+    };
+
+    let scripts = vec![
+        ScriptBuf::from_asm("OP_PUSHBYTES_4 53414b45 OP_DROP OP_IF OP_0 OP_ELSE OP_1 OP_ENDIF")
+            .unwrap(),
+        ScriptBuf::from_asm("OP_PUSHBYTES_4 53414b45 OP_DROP OP_IF OP_1 OP_ELSE OP_0 OP_ENDIF")
+            .unwrap(),
+    ];
+
+    let prevouts = vec![TxOut {
+        value: Amount::ZERO,
+        // Skip creating a pay tot taproot pubkey
+        script_pubkey: ScriptBuf::new_p2a(),
+    }];
+
+    validate(&tx, &prevouts, &scripts).unwrap();
+}
